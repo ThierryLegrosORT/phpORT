69e5a2bfe3b6c3b7205185afb993c8e596a6d74a
Thierry Legros <thierry.legros2@club.fr>
2019-07-30 09:30:39 +0200

    tableau associatif

--- index.php
+++ index.php
@@ -1,24 +1,36 @@
 <?php
-$tab = array(1,2,3);
 
-for($i = 0; $i < sizeof($tab); $i++){
-    echo $tab[$i];
-    echo "<br/>";
-}
+$address = [
+    "Sophie" => [
+        "street" => "Allée des Embruns",
+        "cp" => "33333",
+        "city" => "Pétaouchnok-les-Bains"
+    ],
+    "Jacques" => [
+        "street" => "Allée des Embrouilles",
+        "cp" => "33332",
+        "city" => "Pétaouchnok-City"
+    ],
+    "Marc" => [
+        "street" => "Allée des Noyés",
+        "cp" => "32333",
+        "city" => "Pétaouchnok"
+    ],
+    ];
 
-$tab = ["coucou","les","amis"];
-foreach($tab as $value){
-    echo $value." ";
-    echo "<br/>";
-}
+    $ami = [
+        "Sophie"=>[
+            "name"=>"NoName",
+            "age"=>32,
+            "address"=>$address["Sophie"]
+        ],
+        "Marc"=>[
+            "name"=>"Namose",
+            "age"=>33,
+            "address"=>$address["Marc"]
+        ],
+    ];
 
-echo "<br>";
-
-$tab = [
-    "debut" => "il était une fois...",
-    "fin" => "voila c'est fini"
-];
-
-foreach($tab as $key => $value){
-    echo"$key => $value<br>";
-}
+    echo "<pre>";
+    var_dump($ami["Sophie"]);
+    echo "</pre>";
